d411e6095bd4a688877621c55d215ab82c901402
Alan Lu <alanlu1023@gmail.com>
2017-06-26 12:43:37 -0500

    Delete Koin from migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var Koin = artifacts.require("./AbstractKoin.sol");
 var MetaKoin = artifacts.require("./MetaCoin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Koin);
-  deployer.link(Koin, MetaKoin);
   deployer.deploy(MetaKoin);
 };
